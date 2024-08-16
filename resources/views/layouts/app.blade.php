07146ea5c265cb83b4c1658c0f2b8adbbcc212df
Sabbir Mondol <98119747+devsabbirbd@users.noreply.github.com>
2024-08-16 23:12:10 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,16 +2,6 @@
 <html>
 <head>
     <title>Contact Management</title>
-    <style>
-    .create-contact{
-    display: inline-block;
-    padding: 10px 20px;
-    margin-bottom: 15px;
-    color: #ffffff;
-    background-color: #007bff;
-    border-radius: 5px;
-    text-decoration: none;
-}    </style>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
